54f6603357e3acd62ee5276feb4ccc54f53c3302
Matheus Vitor Fernandes Moreira <matheushifi@gmail.com>
2023-04-19 13:50:00 -0300

    comit inicial

---
+++ index.js
@@ -0,0 +1,43 @@
+/* let a;
+let b;
+let h;
+
+a = window.prompt("Diga o valor do lado 'a' do triângulo:");
+a = Number(a);
+
+b = window.prompt("Diga o valor do lado 'b' do triângulo:")
+b = Number(b);
+
+h = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+console.log("A hipotenusa eh: ", h); */
+////////////////////////////////////////////////////////////////
+/* let count = 0;
+
+document.getElementById('decreaseBtn').onclick = function(){
+    count-=1;
+    document.getElementById("countLabel").innerHTML = count;
+}
+document.getElementById('increaseBtn').onclick = function(){
+    count+=1;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+document.getElementById('resetBtn').onclick = function(){
+    count=0;
+    document.getElementById("countLabel").innerHTML = count;
+} */
+//////////////////////////////////////////////////////////////////
+/* let symbol = window.prompt("Qual simbolo?");
+let linhas = window.prompt("Quantas linhas?");
+let colunas = window.prompt("Quantas colunas?");
+
+for (let i = 1; i <= linhas; i++) {
+  for (let j = 1; j <= colunas; j++) {
+    document.getElementById("myRectangle").innerHTML += symbol;
+  }
+  document.getElementById("myRectangle").innerHTML += "<br>";
+}
+
+console.log("teste"); */
+//////////////////////////////////////////////////////////////////////
